20e476f40fea90f18ae354df14c8484d3ba9158d
breverdbidder <brevardbidderai@gmail.com>
2025-12-23 08:22:04 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    minify: 'esbuild',
+    minify: 'terser',
     rollupOptions: {
       input: {
         main: 'index.html'
